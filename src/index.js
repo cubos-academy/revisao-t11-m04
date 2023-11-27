ad60bcf365ab6537664b495edfcedc763b1b028c
Silas Ribeiro <santorsilas@gmail.com>
2023-11-27 15:09:34 -0300

    alterando rotas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
 
 const express = require('express')
-const rotas = require('./rotas')
 
 const app = express()
 app.use(express.json())
-app.use(rotas)
+app.use(require('./rotas'))
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor rodando em http://localhost:3000')
